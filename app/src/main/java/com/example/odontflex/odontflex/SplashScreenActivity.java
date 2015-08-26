b705eb8b95844eef69a8c7404e83d0ff075e5fef
Valentina <valen92_@hotmail.com>
2015-08-26 16:40:45 -0500

    Commit prueba - update

--- app/src/main/java/com/example/odontflex/odontflex/SplashScreenActivity.java
+++ app/src/main/java/com/example/odontflex/odontflex/SplashScreenActivity.java
@@ -12,7 +12,7 @@ import android.view.Window;
 public class SplashScreenActivity extends Activity {
 
     // Set the duration of the splash screen
-    private static final long SPLASH_SCREEN_DELAY = 4000;
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
