5ab52e5579cee9656634297e17bca84bb7cebb30
Valentina <valen92_@hotmail.com>
2015-09-14 16:54:45 -0500

    Se actualiza navegacion a detalle de CUP. Falta implementar consulta

--- app/src/main/java/com/example/odontflex/odontflex/CUPS.java
+++ app/src/main/java/com/example/odontflex/odontflex/CUPS.java
@@ -4,83 +4,45 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
-public class InfoGeneral extends ActionBarActivity {
+public class CUPS extends AppCompatActivity {
 
     private SlidingPaneLayout mPanes;
     private static final int PARALLAX_SIZE = 30;
-    ListViewAdapter adapter, adaptador;
-    ListView lvInfoGeneral;
+    private String[] mListItems;
+    ListViewAdapter adapter;
+    int tipoCup=0;
+    ListView lvCups;
     String[] opciones = new  String[]{
             "",
             "",
     };
 
-    int[] imgOInfo={
-            10,
-            10,
-    };
-
-    String[] info = new  String[]{
-            "CUPS",
-            "Acerca de Odontflex",
-    };
-
     int[] imgOpciones={
             R.drawable.glyphicons_029_notes_2,
             R.drawable.iconoconsultorio,
-    };
 
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_general);
+        setContentView(R.layout.activity_cups);
         ActionBar actionBar = getSupportActionBar();
         actionBar.show();
 
         actionBar.setDefaultDisplayHomeAsUpEnabled(true);
         mPanes = (SlidingPaneLayout)findViewById(R.id.slidingPane);
-        mPanes.setParallaxDistance(PARALLAX_SIZE);
         ListView list = (ListView)findViewById(R.id.animalList);
         adapter = new ListViewAdapter(this,opciones,imgOpciones);
         list.setAdapter(adapter);
         list.setBackgroundColor(Color.rgb(178, 223, 219));
-
-
-        lvInfoGeneral = (ListView)findViewById(R.id.listViewInfoGeneral);
-        adaptador = new ListViewAdapter(this,info,imgOInfo);
-        lvInfoGeneral.setAdapter(adaptador);
-        lvInfoGeneral.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Intent cups = new Intent(getApplicationContext(),
-                                CUPS.class);
-                        startActivity(cups);
-                        finish();
-                        break;
-                    case 1:
-                        Intent acercade = new Intent(getApplicationContext(),
-                                Acercade.class);
-                        startActivity(acercade);
-                        finish();
-                        break;
-                }
-
-            }
-
-
-        });
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -97,16 +59,28 @@ public class InfoGeneral extends ActionBarActivity {
                 }
 
             }
-
-
         });
 
+        lvCups = (ListView)findViewById(R.id.lvCups);
+        lvCups.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tipoCup = 1 + position;
+                Intent listaCups = new Intent(getApplicationContext(),
+                        ListaCUPS.class);
+                listaCups.putExtra("tipoCup", Integer.toString(tipoCup));
+                startActivity(listaCups);
+                finish();
+
+            }
+        });
+        tipoCup=0;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_info_general, menu);
+        getMenuInflater().inflate(R.menu.menu_cu, menu);
         return true;
     }
 
@@ -156,6 +130,13 @@ public class InfoGeneral extends ActionBarActivity {
         Intent salir = new Intent(getApplicationContext(),
                 MainActivity.class);
         startActivity(salir);
+        finish();
+    }
+
+    public void Atras (View v){
+        Intent atras = new Intent(getApplicationContext(),
+                InfoGeneral.class);
+        startActivity(atras);
         finish();
     }
 }
--- app/src/main/java/com/example/odontflex/odontflex/ListaCUPS.java
+++ app/src/main/java/com/example/odontflex/odontflex/ListaCUPS.java
@@ -4,83 +4,61 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class ListaCUPS extends AppCompatActivity {
 
-public class InfoGeneral extends ActionBarActivity {
-
+    String tipoCup;
     private SlidingPaneLayout mPanes;
     private static final int PARALLAX_SIZE = 30;
-    ListViewAdapter adapter, adaptador;
-    ListView lvInfoGeneral;
+    private String[] mListItems;
+    ListViewAdapter adapter;
+    ListView lvCups;
     String[] opciones = new  String[]{
             "",
             "",
     };
 
-    int[] imgOInfo={
-            10,
-            10,
-    };
-
-    String[] info = new  String[]{
-            "CUPS",
-            "Acerca de Odontflex",
-    };
-
     int[] imgOpciones={
             R.drawable.glyphicons_029_notes_2,
             R.drawable.iconoconsultorio,
+
     };
+    TableLayout tabla;
+    TableLayout cabecera;
+    TableRow.LayoutParams layoutFila;
+    TableRow.LayoutParams layoutId;
+    TableRow.LayoutParams layoutTexto;
+
+    private int MAX_FILAS = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_general);
+        setContentView(R.layout.activity_lista_cups);
+        Intent dato = getIntent();
+        tipoCup = dato.getStringExtra("tipoCup");
+        Toast.makeText(ListaCUPS.this, ""+tipoCup, Toast.LENGTH_SHORT).show();
         ActionBar actionBar = getSupportActionBar();
         actionBar.show();
 
         actionBar.setDefaultDisplayHomeAsUpEnabled(true);
         mPanes = (SlidingPaneLayout)findViewById(R.id.slidingPane);
-        mPanes.setParallaxDistance(PARALLAX_SIZE);
         ListView list = (ListView)findViewById(R.id.animalList);
         adapter = new ListViewAdapter(this,opciones,imgOpciones);
         list.setAdapter(adapter);
         list.setBackgroundColor(Color.rgb(178, 223, 219));
-
-
-        lvInfoGeneral = (ListView)findViewById(R.id.listViewInfoGeneral);
-        adaptador = new ListViewAdapter(this,info,imgOInfo);
-        lvInfoGeneral.setAdapter(adaptador);
-        lvInfoGeneral.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Intent cups = new Intent(getApplicationContext(),
-                                CUPS.class);
-                        startActivity(cups);
-                        finish();
-                        break;
-                    case 1:
-                        Intent acercade = new Intent(getApplicationContext(),
-                                Acercade.class);
-                        startActivity(acercade);
-                        finish();
-                        break;
-                }
-
-            }
-
-
-        });
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -97,16 +75,22 @@ public class InfoGeneral extends ActionBarActivity {
                 }
 
             }
-
-
         });
 
+        tabla = (TableLayout)findViewById(R.id.tabla);
+        cabecera = (TableLayout)findViewById(R.id.cabecera);
+        layoutFila = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT);
+        layoutId = new TableRow.LayoutParams(120,50);
+        layoutTexto = new TableRow.LayoutParams(460,50);
+        agregarCabecera();
+        agregarFilasTabla();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_info_general, menu);
+        getMenuInflater().inflate(R.menu.menu_lista_cu, menu);
         return true;
     }
 
@@ -157,5 +141,69 @@ public class InfoGeneral extends ActionBarActivity {
                 MainActivity.class);
         startActivity(salir);
         finish();
+    }
+
+    public void Atras (View v){
+        Intent atras = new Intent(getApplicationContext(),
+                CUPS.class);
+        startActivity(atras);
+        finish();
+    }
+
+    public void agregarCabecera(){
+        TableRow fila;
+        TextView txtId;
+        TextView txtNombre;
+
+        fila = new TableRow(this);
+        fila.setLayoutParams(layoutFila);
+
+        txtId = new TextView(this);
+        txtNombre = new TextView(this);
+
+        txtId.setText("Codigo");
+        txtId.setGravity(Gravity.CENTER_HORIZONTAL);
+        txtId.setTextAppearance(this, R.style.etiqueta);
+        txtId.setLayoutParams(layoutId);
+
+        txtNombre.setText("Nombre");
+        txtNombre.setGravity(Gravity.CENTER_HORIZONTAL);
+        txtNombre.setTextAppearance(this, R.style.etiqueta);
+        txtNombre.setLayoutParams(layoutTexto);
+
+        fila.addView(txtId);
+        fila.addView(txtNombre);
+        cabecera.addView(fila);
+    }
+
+    public void agregarFilasTabla(){
+
+        TableRow fila;
+        TextView txtId;
+        TextView txtNombre;
+
+        for(int i = 0;i<MAX_FILAS;i++){
+            int posicion = i + 1;
+            fila = new TableRow(this);
+            fila.setLayoutParams(layoutFila);
+
+            txtId = new TextView(this);
+            txtNombre = new TextView(this);
+
+            txtId.setText(String.valueOf(posicion));
+            txtId.setGravity(Gravity.CENTER_HORIZONTAL);
+            txtId.setTextAppearance(this, R.style.etiqueta);
+            txtId.setLayoutParams(layoutId);
+
+            txtNombre.setText("Texto " + posicion);
+            txtNombre.setGravity(Gravity.CENTER_HORIZONTAL);
+            txtNombre.setTextAppearance(this, R.style.etiqueta);
+            txtNombre.setLayoutParams(layoutTexto);
+
+            fila.addView(txtId);
+            fila.addView(txtNombre);
+
+            tabla.addView(fila);
+        }
     }
 }
