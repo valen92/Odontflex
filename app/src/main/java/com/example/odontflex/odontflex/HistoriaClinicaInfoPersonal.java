2dbe2d01be52a5c5b34f0f0826dd38b20668874c
Valentina <valen92_@hotmail.com>
2015-09-16 14:49:47 -0500

    Se añade slidePanel en infoPersonal paciente no existe

--- app/src/main/java/com/example/odontflex/odontflex/HistoriaClinicaInfoPersonal.java
+++ app/src/main/java/com/example/odontflex/odontflex/HistoriaClinicaInfoPersonal.java
@@ -1,12 +1,33 @@
 package com.example.odontflex.odontflex;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class HistoriaClinicaInfoPersonal extends AppCompatActivity {
+
+    private SlidingPaneLayout mPanes;
+    private static final int PARALLAX_SIZE = 30;
+    private String[] mListItems;
+    ListViewAdapter adapter;
+    String[] opciones = new  String[]{
+            "",
+            ""
+    };
+
+    int[] imgOpciones={
+            R.drawable.iconoconsultorio,
+            R.drawable.glyphicons_195_circle_info
+
+    };
 
     String idPaciente, idOdontologo;
 
@@ -17,6 +38,34 @@ public class HistoriaClinicaInfoPersonal extends AppCompatActivity {
         Intent dato = getIntent();
         idOdontologo = dato.getStringExtra("idOdontologo");
         idPaciente = dato.getStringExtra("idPaciente");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.show();
+
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        mPanes = (SlidingPaneLayout)findViewById(R.id.slidingPane);
+        ListView list = (ListView)findViewById(R.id.animalList);
+        adapter = new ListViewAdapter(this,opciones,imgOpciones);
+        list.setAdapter(adapter);
+        list.setBackgroundColor(Color.rgb(178, 223, 219));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+
+                        break;
+                    case 1:
+                        Intent infoGeneral = new Intent(getApplicationContext(),
+                                InfoGeneral.class);
+                        startActivity(infoGeneral);
+                        finish();
+                        break;
+                }
+
+            }
+
+
+        });
 
     }
 
@@ -35,10 +84,37 @@ public class HistoriaClinicaInfoPersonal extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.opciones:
+                if (mPanes.closePane()){
+                    closePane();
+                } else {
+                    openPane();
+                }
+                break;
+            case  R.id.salir:
+                Salir();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPane(){
+        mPanes.openPane();
+    }
+
+    private void closePane(){
+        mPanes.closePane();
+    }
+
+    public void onBackPressed(){
+    }
+
+    public void Salir (){
+        Intent salir = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(salir);
+        finish();
     }
 }
